3c78fc598e3b962e3817670eeb4368ed4e9e2f2d
sreeharics011999 <sreeharishambu2000@gmail.com>
2023-07-19 22:38:37 +0530

    contact card changes

--- myapp/src/Component/Contact.js
+++ myapp/src/Component/Contact.js
@@ -11,7 +11,7 @@ let Contact=()=>{
                 <img className={styles.star} src={star}/>
                 <p className={styles.para1}>Google 4.9 * Rated Institute</p>
                 <div>
-                     <div className="container bg-light" style={{marginLeft:"22%",width:"56vw",height:"62vh", backgroundImage:'url("https://scopeindia.org/images/si-call-back-bg.jpg")'}}>
+                     <div className="container bg-light" style={{padding:"3%",width:"55%", backgroundImage:'url("https://scopeindia.org/images/si-call-back-bg.jpg")'}}>
                         <div className="row">
                             <div className="col-lg-6 col-md-8">
                             <NavLink to="tel:+919745936073"><img src={formimg} className={styles.imgfrm}/></NavLink>
@@ -26,18 +26,60 @@ let Contact=()=>{
                                 <input type="number" class="form-control" placeholder="Enter your phone no." id="exampleInputPassword1"/>
                                 </div>
                                 <div class="d-grid   mx-auto mt-4  ">
-                                  <button class="btn btn-primary pb-2" type="button">Call Me</button>
+                                  <button class="btn  pb-2" style={{backgroundColor:"#00FF00",color:"white",fontWeight:"900"}} type="button">Call Me</button>
                                </div>
-                               <div class="d-grid gap-3  mx-auto mt-5 pt-5  ">
-                                <button class="btn btn-primary mb-2 pb-2" size={55} type="button">Recent placements</button>
-                                <button class="btn btn-info pb-2" type="button">Courses</button>
-                                <button class="btn btn-success pb-2 " type="button">Register Now!</button>                               
+                               <div class="d-grid gap-3  mx-auto mt-4 pt-5  ">
+                                <button class="btn  mb-2 pb-2" style={{backgroundColor:" #7c62bd",color:"white",borderRadius:"6px",borderColor:"#5c1eff"}} size={55} type="button">Recent placements</button>
+                                <button class="btn  pb-2" style={{backgroundColor:" #62bdb4",color:"white",borderRadius:"6px",borderColor:"#35e9d7"}}  type="button">Courses</button>
+                                <button class="btn  pb-2 " style={{backgroundColor:"  #5ead5e",color:"white",borderRadius:"6px",borderColor:"#32d732"}}  type="button">Register Now!</button>                               
                                </div>
                              </form>
                             </div>
                         </div>
                      </div>
-                  
+                  </div>
+                <div className="container">
+                <h2 style={{textAlign:"start"}}>Location</h2>
+                <div className="row">
+                  <div className="col-lg-4">
+                     <div className="card" style={{width: "18rem", backgroundColor:"#041a6b "}}>
+                       <div className="card-body">
+                         <h5 className="card-title">Card title</h5>
+                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                     <div className="card" style={{width: "18rem"}}>
+                       <div className="card-body">
+                         <h5 className="card-title">Card title</h5>
+                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                     <div className="card" style={{width: "18rem"}}>
+                       <div className="card-body">
+                         <h5 className="card-title">Card title</h5>
+                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                         <NavLink to="#" className="card-link">Another link</NavLink>
+                      </div>
+                    </div>
+                  </div>
+                    
+                </div>
+
                 </div>
             </div>
            
--- myapp/src/Component/Home.js
+++ myapp/src/Component/Home.js
@@ -6,11 +6,11 @@ import Carosal from "./Carosal.jpg"
 let Home=()=>{
     return(
         <div>
-            <div style={{backgroundColor:"#071e60",height:"10px"}}></div>
+            <div style={{backgroundColor:"#071e60",height:"1vh"}}></div>
             <img src={Scopebackground} alt="scopebackground" className={styles.homeimg1}/>
             <img src={Scope} alt="scope" className="position-absolute top-50 start-50 w-auto h-auto translate-middle"/>
             <img src={Subbg}/>
-            <h1>our home page is it</h1>
+            
 
         </div>
     )
