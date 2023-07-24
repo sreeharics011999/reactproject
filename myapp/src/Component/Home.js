6d5ca60b00197ef4d41a2a7f9fc19927edc1329e
unknown <sreeharishambu2000@gmail.com>
2023-07-25 01:21:54 +0530

    home added1

--- myapp/src/Component/Home.js
+++ myapp/src/Component/Home.js
@@ -1,16 +1,20 @@
 import Scope from "./images/Scope.png"
-import Scopebackground from "./images/Scopebackground.jpg"
 import Subbg from "./images/Subbg.png"
-import styles from "./home.module.css"
+import "./home.css"
 import Carosal from "./images/Carosal.jpg"
 let Home=()=>{
     return(
         <div>
-            <div style={{backgroundColor:"#071e60",height:"1vh"}}></div>
+            {/* <div style={{backgroundColor:"#071e60",height:"1vh"}}></div>
             <img src={Scopebackground} alt="scopebackground" className={styles.homeimg1}/>
             <img src={Scope} alt="scope" className="position-absolute top-50 start-50 w-auto h-auto translate-middle"/>
-            <img src={Subbg}/>
-            
+            <img src={Subbg}/> */}
+            <div clasName="background">
+                <div clasName="container">
+                <img src={Scope} alt="scope" className="scopeimg"/>
+                </div>
+
+            </div>
 
         </div>
     )
